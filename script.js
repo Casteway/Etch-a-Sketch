ec285b54e10d74018f04b82178359ed5213e0350
Krystian <napier2211@gmail.com>
2026-09-15 08:57:10 +0200

    added hover eventListener

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ for (let i = 1; i <= 256; i++) {
   let newDiv = document.createElement("div");
   container.append(newDiv);
 }
+
+container.addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "purple";
+});
