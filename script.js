d137c9912d6e1c6f08e99244d2f5258779495930
Krystian <napier2211@gmail.com>
2026-09-15 09:15:15 +0200

    added coloring each grid when passing over with a mouse

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const container = document.querySelector(".container");
 
 for (let i = 1; i <= 256; i++) {
   let newDiv = document.createElement("div");
+  newDiv.classList.add("gridDiv");
   container.append(newDiv);
 }
 
 container.addEventListener("mouseover", (event) => {
-  event.target.style.backgroundColor = "purple";
+  if (event.target.classList.contains("gridDiv")) {
+    event.target.style.backgroundColor = "purple";
+  }
 });
