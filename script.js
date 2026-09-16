dd2c2920d8e7a3c6aa289843a7052d88570de1ee
Krystian <napier2211@gmail.com>
2026-09-16 15:32:12 +0200

    added function wraper around creating grid

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector(".container");
 
-for (let i = 1; i <= 256; i++) {
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("gridDiv");
-  newDiv.style.width = `${640 / Math.sqrt(256)}px`;
-  newDiv.style.height = `${640 / Math.sqrt(256)}px`;
-  container.append(newDiv);
+function createGrid(size) {
+  container.innerHTML = "";
+  for (let i = 1; i <= size * size; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("gridDiv");
+    newDiv.style.width = `${640 / Math.sqrt(size * size)}px`;
+    newDiv.style.height = `${640 / Math.sqrt(size * size)}px`;
+    container.append(newDiv);
+  }
 }
 
 container.addEventListener("mouseover", (event) => {
@@ -27,6 +30,10 @@ function changeGridSize(event) {
   newSize = prompt("Provide Number of squares per side for the new grid");
   if (newSize > 100) {
     newGridSize = prompt("Sorry, max  size is 100");
+  } else if (newSize <= 1) {
+    newGridSize = prompt("Sorry, min size is 2");
   }
   return newSize;
 }
+
+createGrid(16);
